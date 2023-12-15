7f5981ac59241d66077734d4d85a9f616c7956a2
Erick <danbencb08@outlook.es>
2023-12-15 00:04:43 -0500

    Variables

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-const API_URL= 'http://localhost:8000'
+const API_URL= 'https://back-undac.onrender.com'
 const API_SISTEMA=  '/api/sistema'
 const API_ESTUDIANTE=  '/api/estudiantes'
 const API_ADMINISTRADOR= '/api/administrador'
